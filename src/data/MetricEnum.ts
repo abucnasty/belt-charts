369e62ee1406b83eda081720823ae8e28eb65373
AHumanMerc <50553851+AHumanMerc@users.noreply.github.com>
2025-10-15 23:06:30 -0600

    Start of refactoring/reworking main script

--- src/data/MetricEnum.ts
+++ src/data/MetricEnum.ts
@@ -1,168 +1,332 @@
-export const MetricEnum = {
+const WholeUpdate = {
     WHOLE_UPDATE: {
         name: "wholeUpdate",
         description: "Whole Update"
-    },
+    }
+} as const;
+
+const LatencyUpdate = {
     LATENCY_UPDATE: {
         name: "latencyUpdate",
         description: "Latency Update"
-    },
+    }
+} as const;
+
+const GameUpdate = {
     GAME_UPDATE: {
         name: "gameUpdate",
         description: "Game Update"
-    },
+    }
+} as const;
+
+const PlanetsUpdate = {
     PLANETS_UPDATE: {
         name: "planetsUpdate",
         description: "Planets Update"
-    },
+    }
+} as const;
+
+const ControlBehaviorUpdate = {
     CONTROL_BEHAVIOR_UPDATE: {
         name: "controlBehaviorUpdate",
         description: "Control Behavior Update"
-    },
+    }
+} as const;
+
+const TransportLinesUpdate = {
     TRANSPORT_LINES_UPDATE: {
         name: "transportLinesUpdate",
         description: "Transport Lines Update"
-    },
+    }
+} as const;
+
+const ElectricHeatFluidCircuitUpdate = {
     ELECTRIC_HEAT_FLUID_CIRCUIT_UPDATE: {
         name: "electricHeatFluidCircuitUpdate",
         description: "Electric/Heat/Fluid Circuit Update"
-    },
+    }
+} as const;
+
+const ElectricNetworkUpdate = {
     ELECTRIC_NETWORK_UPDATE: {
         name: "electricNetworkUpdate",
         description: "Electric Network Update"
-    },
+    }
+} as const;
+
+const HeatNetworkUpdate = {
     HEAT_NETWORK_UPDATE: {
         name: "heatNetworkUpdate",
         description: "Heat Network Update"
-    },
+    }
+} as const;
+
+const FluidFlowUpdate = {
     FLUID_FLOW_UPDATE: {
         name: "fluidFlowUpdate",
         description: "Fluid Flow Update"
-    },
+    }
+} as const;
+
+const EntityUpdate = {
     ENTITY_UPDATE: {
         name: "entityUpdate",
         description: "Entity Update"
-    },
+    }
+} as const;
+
+const LightningUpdate = {
     LIGHTNING_UPDATE: {
         name: "lightningUpdate",
         description: "Lightning Update"
-    },
+    }
+} as const;
+
+const TileHeatingUpdate = {
     TILE_HEATING_UPDATE: {
         name: "tileHeatingUpdate",
         description: "Tile Heating Update"
-    },
+    }
+} as const;
+
+const ParticleUpdate = {
     PARTICLE_UPDATE: {
         name: "particleUpdate",
         description: "Particle Update"
-    },
+    }
+} as const;
+
+const MapGenerator = {
     MAP_GENERATOR: {
         name: "mapGenerator",
         description: "Map Generator"
-    },
+    }
+} as const;
+
+const MapGeneratorBasicTilesSupportApply = {
     MAP_GENERATOR_BASIC_TILES_SUPPORT_COMPUTE: {
         name: "mapGeneratorBasicTilesSupportCompute",
         description: "Map Generator Basic Tiles Support Compute"
-    },
+    }
+} as const;
+
+const MapGeneratorBasicTilesSupportCompute = {
     MAP_GENERATOR_BASIC_TILES_SUPPORT_APPLY: {
         name: "mapGeneratorBasicTilesSupportApply",
         description: "Map Generator Basic Tiles Support Apply"
-    },
+    }
+} as const;
+
+const MapGeneratorCorrectedTilesApply = {
     MAP_GENERATOR_CORRECTED_TILES_PREPARE: {
         name: "mapGeneratorCorrectedTilesPrepare",
         description: "Map Generator Corrected Tiles Prepare"
-    },
+    }
+} as const;
+
+const MapGeneratorCorrectedTilesCompute = {
     MAP_GENERATOR_CORRECTED_TILES_COMPUTE: {
         name: "mapGeneratorCorrectedTilesCompute",
         description: "Map Generator Corrected Tiles Compute"
-    },
+    }
+} as const;
+
+const MapGeneratorCorrectedTilesPrepare = {
     MAP_GENERATOR_CORRECTED_TILES_APPLY: {
         name: "mapGeneratorCorrectedTilesApply",
         description: "Map Generator Corrected Tiles Apply"
-    },
-    MAP_GENERATOR_VARIATIONS: {
-        name: "mapGeneratorVariations",
-        description: "Map Generator Variations"
-    },
-    MAP_GENERATOR_ENTITIES_PREPARE: {
-        name: "mapGeneratorEntitiesPrepare",
-        description: "Map Generator Entities Prepare"
-    },
-    MAP_GENERATOR_ENTITIES_COMPUTE: {
-        name: "mapGeneratorEntitiesCompute",
-        description: "Map Generator Entities Compute"
-    },
+    }
+} as const;
+
+const MapGeneratorEntitiesApply = {
     MAP_GENERATOR_ENTITIES_APPLY: {
         name: "mapGeneratorEntitiesApply",
         description: "Map Generator Entities Apply"
-    },
+    }
+}
+
+const MapGeneratorEntitiesCompute = {
+    MAP_GENERATOR_ENTITIES_COMPUTE: {
+        name: "mapGeneratorEntitiesCompute",
+        description: "Map Generator Entities Compute"
+    }
+} as const;
+
+const MapGeneratorEntitiesPrepare = {
+    MAP_GENERATOR_ENTITIES_PREPARE: {
+        name: "mapGeneratorEntitiesPrepare",
+        description: "Map Generator Entities Prepare"
+    }
+} as const;
+
+const MapGeneratorVariations = {
+    MAP_GENERATOR_VARIATIONS: {
+        name: "mapGeneratorVariations",
+        description: "Map Generator Variations"
+    }
+} as const;
+
+const SpacePlatforms = {
     SPACE_PLATFORMS: {
         name: "spacePlatforms",
         description: "Space Platforms"
-    },
+    }
+} as const;
+
+const CollectorNavMesh = {
     COLLECTOR_NAV_MESH: {
         name: "collectorNavMesh",
         description: "Collector Nav Mesh"
-    },
+    }
+} as const;
+
+const CollectorNavMeshPathfinding = {
     COLLECTOR_NAV_MESH_PATHFINDING: {
         name: "collectorNavMeshPathfinding",
         description: "Collector Nav Mesh Pathfinding"
-    },
+    }
+} as const;
+
+const CollectorNavMeshRaycast = {
     COLLECTOR_NAV_MESH_RAYCAST: {
         name: "collectorNavMeshRaycast",
         description: "Collector Nav Mesh Raycast"
-    },
+    }
+} as const;
+
+const CRCComputation = {
     CRC_COMPUTATION: {
         name: "crcComputation",
         description: "Crc Computation"
-    },
+    }
+} as const;
+
+const ConsistencyScraper = {
     CONSISTENCY_SCRAPER: {
         name: "consistencyScraper",
         description: "Consistency Scraper"
-    },
+    }
+} as const;
+
+const LogisticManagerUpdate = {
     LOGISTIC_MANAGER_UPDATE: {
         name: "logisticManagerUpdate",
         description: "Logistic Manager Update"
-    },
+    }
+} as const;
+
+const ConstructionManagerUpdate = {
     CONSTRUCTION_MANAGER_UPDATE: {
         name: "constructionManagerUpdate",
         description: "Construction Manager Update"
-    },
+    }
+} as const;
+
+const PathFinder = {
     PATH_FINDER: {
         name: "pathFinder",
         description: "Path Finder"
-    },
+    }
+} as const;
+
+const Trains = {
     TRAINS: {
         name: "trains",
         description: "Trains"
-    },
+    }
+} as const;
+
+const TrainPathFinder = {
     TRAIN_PATH_FINDER: {
         name: "trainPathFinder",
         description: "Train Path Finder"
-    },
+    }
+} as const;
+
+const Commander = {
     COMMANDER: {
         name: "commander",
         description: "Commander"
-    },
-    CHART_REFRESH: {
-        name: "chartRefresh",
-        description: "Chart Refresh"
-    },
+    }
+} as const;
+
+const LuaGarbageIncremental = {
     LUA_GARBAGE_INCREMENTAL: {
         name: "luaGarbageIncremental",
         description: "Lua Garbage Incremental"
-    },
+    }
+} as const;
+
+const ChartRefresh = {
+    CHART_REFRESH: {
+        name: "chartRefresh",
+        description: "Chart Refresh"
+    }
+} as const;
+
+const ChartUpdate = {
     CHART_UPDATE: {
         name: "chartUpdate",
         description: "Chart Update"
-    },
+    }
+} as const;
+
+const ScriptUpdate = {
     SCRIPT_UPDATE: {
         name: "scriptUpdate",
         description: "Script Update"
-    },
+    }
+} as const;
+
+const Other = {
     OTHER: {
         name: "other",
         description: "Other"
     }
+} as const;
+
+export const MetricEnum = {
+    ...WholeUpdate,
+    ...LatencyUpdate,
+    ...GameUpdate,
+    ...PlanetsUpdate,
+    ...ControlBehaviorUpdate,
+    ...TransportLinesUpdate,
+    ...ElectricHeatFluidCircuitUpdate,
+    ...ElectricNetworkUpdate,
+    ...HeatNetworkUpdate,
+    ...FluidFlowUpdate,
+    ...EntityUpdate,
+    ...LightningUpdate,
+    ...TileHeatingUpdate,
+    ...ParticleUpdate,
+    ...MapGenerator,
+    ...MapGeneratorBasicTilesSupportApply,
+    ...MapGeneratorBasicTilesSupportCompute,
+    ...MapGeneratorCorrectedTilesApply,
+    ...MapGeneratorCorrectedTilesCompute,
+    ...MapGeneratorCorrectedTilesPrepare,
+    ...MapGeneratorEntitiesApply,
+    ...MapGeneratorEntitiesCompute,
+    ...MapGeneratorEntitiesPrepare,
+    ...MapGeneratorVariations,
+    ...SpacePlatforms,
+    ...CollectorNavMesh,
+    ...CollectorNavMeshPathfinding,
+    ...CollectorNavMeshRaycast,
+    ...CRCComputation,
+    ...ConsistencyScraper,
+    ...LogisticManagerUpdate,
+    ...ConstructionManagerUpdate,
+    ...PathFinder,
+    ...Trains,
+    ...TrainPathFinder,
+    ...Commander,
+    ...LuaGarbageIncremental,
+    ...ChartRefresh,
+    ...ChartUpdate,
+    ...ScriptUpdate,
+    ...Other
 } as const;
 
 export type MetricEnum = typeof MetricEnum[keyof typeof MetricEnum];
--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,120 @@ import { Canvas } from "skia-canvas"
 import { Chart, LinearScale, CategoryScale, registerables } from "chart.js";
 import fsp from 'node:fs/promises';
 import { BenchmarkAggregateRunResult, parseBenchmarkAggregatesPerRunResultFromCsv, saveBenchmarkAggregateRunResultsToCsv } from "./data/BenchmarkAggregateResult";
+import chalk from "chalk";
+import { intro, text, select, outro } from "@clack/prompts";
 
-Chart.register(
-  BoxPlotController,
-  BoxAndWiskers,
-  LinearScale,
-  CategoryScale,
-  ...registerables
-);
+async function summaryChartProcess(
+  files: string[],
+  trimPrefix: string,
+  removeFirstTicks: number,
+  maxTicks: number,
+  metrics: MetricEnum[],
+  summaryTable: boolean,
+  aggregationStrategy: AggregationStrategy,
+  width: number,
+  height: number,
+  outputFile: string
+) {
+  
+  const aggregateResults: BenchmarkAggregateRunResult[] = []
+  for (const file of files) {
+    console.log(`Processing file: ${file}`);
+    let result: BenchmarkAggregateRunResult = await parseBenchmarkAggregatesPerRunResultFromCsv(file, removeFirstTicks, maxTicks, metrics);
+
+    if (trimPrefix && result.fileName.startsWith(trimPrefix)) {
+      result.fileName = result.fileName.slice(trimPrefix.length);
+    }
+    aggregateResults.push(result);
+  }
+  const config = createSummaryChartConfiguration(aggregateResults, {
+    metrics: metrics,
+    includeTable: summaryTable,
+    aggregationStrategy: aggregationStrategy
+  });
+  console.log("Chart configuration created.");
+  const canvas = new Canvas(width, height)
+  const chart = new Chart(
+    canvas as any,
+    config
+  )
+  const imageBuffer = await canvas.toBuffer("png");
+
+  await fsp.writeFile(outputFile, imageBuffer);
+  console.log(`Summary chart with table saved to ${outputFile}`);
+  chart.destroy()
+  return;
+};
+
+async function barOrLineChartProcess(
+  files: string[],
+  removeFirstTicks: number,
+  trimPrefix: string,
+  maxUpdate: number,
+  maxTicks: number,
+  type: "bar" | "line",
+  aggregationStrategy: AggregationStrategy,
+  tickWindowAggregation: number,
+  outputFile: string,
+  width: number,
+  height: number,
+) {
+  const benchmarkResults: BenchmarkTickResult[] = [];
+  for (const file of files) {
+    console.log(`Processing file: ${file}`);
+    let result: BenchmarkTickResult = await parseBenchmarkAveragePerTickResultFromCsv(file);
+
+    if (removeFirstTicks > 0) {
+      result = ignoreFirstTicksFromResult(result, removeFirstTicks);
+    }
+
+    if (trimPrefix && result.fileName.startsWith(trimPrefix)) {
+      result.fileName = result.fileName.slice(trimPrefix.length);
+    }
+    benchmarkResults.push(result);
+  }
+  let maxWholeUpdate = 0;
+  if (maxUpdate) {
+    maxWholeUpdate = maxUpdate * 1000
+  } else {
+    benchmarkResults.forEach(result => result.metricTickStats.get(MetricEnum.WHOLE_UPDATE.name).forEach(metricValue => {
+      maxWholeUpdate = Math.max(maxWholeUpdate, nanoToMicro(metricValue.maximum))
+    }))
+  }
+  const configurations = benchmarkResults.map(result => {
+    return {
+      result: result,
+      config: createLineChartForMetrics(result, {
+        maxTicks: maxTicks,
+        maxUpdateValue: maxWholeUpdate,
+        type: type,
+        aggregationStrategy,
+        tickWindow: tickWindowAggregation
+      })
+    }
+  })
+  console.log("Chart configurations created.");
+
+  const fileNameWithoutExt = outputFile.replace(/\.[^/.]+$/, "")
+
+  configurations.forEach(async ({ result, config }) => {
+    const canvas = new Canvas(width, height)
+    const chart = new Chart(
+      canvas as any,
+      config
+    )
+    const imageBuffer = await canvas.toBuffer("png");
+
+    const fileName = `${fileNameWithoutExt}_${result.fileName}.png`
+
+    await fsp.writeFile(fileName, imageBuffer);
+    chart.destroy()
+    console.log(`Metric Line Chart Generated for ${fileName}`);
+  });
+
+
+  return;
+};
 
 const program = new Command();
 
@@ -50,18 +156,34 @@ program
   }, MetricRegistryInstance.all())
   .option("-a, --aggregate-strategy <average | minimum | maximum | median | standard_deviation", "aggregate the runs by either minimum per tick or average per tick", "average")
   .action(async (pattern, options) => {
-    const width: number = options.width;
+    intro(chalk.cyan('Welcome to chart-gen!'));
+
+    const aggregationStrategy: AggregationStrategy = aggregationStrategyFromString(options.aggregateStrategy);
     const height: number = options.height;
+    const maxTicks: number = options.maxTicks;
+    const maxUpdate: number = options.maxUpdate;
     const outputFile: string = path.resolve(process.cwd(), options.output);
     const removeFirstTicks: number = options.removeFirstTicks;
-    const maxTicks: number = options.maxTicks;
-    const type: string = options.type;
+    const summaryTable: boolean = options.summaryTable;
+    const tickWindowAggregation: number = options.tickWindowAggregation;
     const trimPrefix = options.trimPrefix;
-    const tickWindowAggregation: number = options.tickWindowAggregation
-    const aggregationStrategy: AggregationStrategy = aggregationStrategyFromString(options.aggregateStrategy)
+    const type: string = options.type;
+    const width: number = options.width;
 
     const metrics: MetricEnum[] = options.metrics
     console.debug(options)
+
+    if (!pattern) {
+      pattern = (await text({
+        message: 'What glob pattern would you like to use for CSV files?',
+        initialValue: './data/*.csv',
+        validate(value) {
+          if (value.length === 0) {
+            return 'You must provide some kind of pattern for CSV file matching'
+          }
+        }
+      })) as string;
+    }
 
     const files = globSync(pattern);
     if (files.length === 0) {
@@ -69,94 +191,35 @@ program
       process.exit(1);
     }
 
-
-
     if (type == "summary") {
-      const aggregateResults: BenchmarkAggregateRunResult[] = []
-      for (const file of files) {
-        console.log(`Processing file: ${file}`);
-        let result: BenchmarkAggregateRunResult = await parseBenchmarkAggregatesPerRunResultFromCsv(file, removeFirstTicks, maxTicks, metrics);
-
-        if (trimPrefix && result.fileName.startsWith(trimPrefix)) {
-          result.fileName = result.fileName.slice(trimPrefix.length);
-        }
-        aggregateResults.push(result);
-      }
-      const config = createSummaryChartConfiguration(aggregateResults, {
-        metrics: metrics,
-        includeTable: options.summaryTable,
-        aggregationStrategy: aggregationStrategy
-      });
-      console.log("Chart configuration created.");
-      const canvas = new Canvas(width, height)
-      const chart = new Chart(
-        canvas as any,
-        config
+      summaryChartProcess(
+        files,
+        trimPrefix,
+        removeFirstTicks,
+        maxTicks,
+        metrics,
+        summaryTable,
+        aggregationStrategy,
+        width,
+        height,
+        outputFile,
       )
-      const imageBuffer = await canvas.toBuffer("png");
-
-      await fsp.writeFile(outputFile, imageBuffer);
-      console.log(`Summary chart with table saved to ${outputFile}`);
-      chart.destroy()
-      return;
     }
 
     if (type == "line" || type == "bar") {
-      const benchmarkResults: BenchmarkTickResult[] = [];
-      for (const file of files) {
-        console.log(`Processing file: ${file}`);
-        let result: BenchmarkTickResult = await parseBenchmarkAveragePerTickResultFromCsv(file);
-
-        if (removeFirstTicks > 0) {
-          result = ignoreFirstTicksFromResult(result, removeFirstTicks);
-        }
-
-        if (trimPrefix && result.fileName.startsWith(trimPrefix)) {
-          result.fileName = result.fileName.slice(trimPrefix.length);
-        }
-        benchmarkResults.push(result);
-      }
-      let maxWholeUpdate = 0;
-      if (options.maxUpdate) {
-        maxWholeUpdate = options.maxUpdate * 1000
-      } else {
-        benchmarkResults.forEach(result => result.metricTickStats.get(MetricEnum.WHOLE_UPDATE.name).forEach(metricValue => {
-          maxWholeUpdate = Math.max(maxWholeUpdate, nanoToMicro(metricValue.maximum))
-        }))
-      }
-      const configurations = benchmarkResults.map(result => {
-        return {
-          result: result,
-          config: createLineChartForMetrics(result, {
-            maxTicks: options.maxTicks,
-            maxUpdateValue: maxWholeUpdate,
-            type: type,
-            aggregationStrategy,
-            tickWindow: tickWindowAggregation
-          })
-        }
-      })
-      console.log("Chart configurations created.");
-
-      const fileNameWithoutExt = outputFile.replace(/\.[^/.]+$/, "")
-
-      configurations.forEach(async ({ result, config }) => {
-        const canvas = new Canvas(width, height)
-        const chart = new Chart(
-          canvas as any,
-          config
-        )
-        const imageBuffer = await canvas.toBuffer("png");
-
-        const fileName = `${fileNameWithoutExt}_${result.fileName}.png`
-
-        await fsp.writeFile(fileName, imageBuffer);
-        chart.destroy()
-        console.log(`Metric Line Chart Generated for ${fileName}`);
-      });
-
-
-      return;
+      barOrLineChartProcess(
+        files,
+        removeFirstTicks,
+        trimPrefix,
+        maxUpdate,
+        maxTicks,
+        type,
+        aggregationStrategy,
+        tickWindowAggregation,
+        outputFile,
+        width,
+        height
+      )
     }
 
     if (type == "boxplot") {
